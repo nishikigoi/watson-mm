c74ba8d43f041d6ecbe793745cddeedaeb87d430
nishikigoi <nishikigoi04@gmail.com>
2016-10-12 20:58:25 +0900

    modify audio link

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ post '/callback' do
         # }
         message = {
           "type": "audio",
-          "originalContentUrl": "https://www.maruhan.co.jp/yosakoi/60sec.m4a",
+          "originalContentUrl": "http://localhost:8888/60sec.m4a",
           "duration": 60000
         }
         client.reply_message(event['replyToken'], message)
